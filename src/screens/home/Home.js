ca911c39f89874f4c91b0257f37d49b28c160a39
Davi Costa <davim.costa@outlook.com>
2023-10-31 22:04:46 -0300

    Tela inicial :construction:

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,18 +1,32 @@
 import { Text } from "react-native-paper"
-import { ImageBackground, StyleSheet } from "react-native";
+import { ImageBackground, StyleSheet, View } from "react-native";
 import Localizacao from "../../components/Localizacao";
+import { useFonts, VT323_400Regular } from '@expo-google-fonts/vt323';
 
 
-const Home = () => { 
+const Home = () => {
 
   const localizacaoData = Localizacao();
   console.log(localizacaoData.apiData)
+
+  let [fontsLoaded] = useFonts({
+    VT323_400Regular,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    
-      <ImageBackground source={require(`../../../assets/background/dia.png`)} style={styles.imageBackground} >
-        <Text>{localizacaoData?.apiData?.location?.name}</Text> 
-        <Text>{localizacaoData?.apiData?.current?.feelslike_c}</Text> 
-      </ImageBackground>
+    <ImageBackground source={require(`../../../assets/background/noiteNublado.png`)} style={styles.imageBackground} >
+      <View style={styles.temperatura}>
+        <Text style={{ fontFamily: 'VT323_400Regular', fontSize: 80, color: 'white', paddingBottom: 0  }}>{localizacaoData?.apiData?.location?.name}</Text>
+        <Text style={{ fontFamily: 'VT323_400Regular', fontSize: 150, color: 'white', paddingTop: 0}}>{localizacaoData?.apiData?.current?.feelslike_c}°</Text>
+
+        <Text style={{ fontFamily: 'VT323_400Regular', fontSize: 40, color: '#bcbcbc', marginTop: 28}}>{localizacaoData?.apiData?.current?.condition.text}</Text>
+
+      </View>
+    </ImageBackground>
   )
 }
 
@@ -20,11 +34,19 @@ export default Home
 
 const styles = StyleSheet.create({
 
-imageBackground: {
+  imageBackground: {
     flex: 1,
     resizeMode: "cover",
     justifyContent: "center",
     alignItems: "center",
     width: "100%"
+  },
+
+  temperatura: {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center",
+    paddingTop: 50,
+    gap: -68
   }
 });
