4d2c83d5ab05d922dc2a8ac593d4c5012af85216
Amanda Viória <amandavic7576@gmail.com>
2023-11-18 00:48:25 -0300

    estilizando viewInfo

--- src/screens/weather/Weather.js
+++ src/screens/weather/Weather.js
@@ -1,28 +1,28 @@
-import { ImageBackground, StyleSheet, Text, TouchableOpacity, View, ScrollView } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import Api from '../../services/Api'
+import React, { useEffect, useState } from 'react';
+import { ImageBackground, StyleSheet, Text, TouchableOpacity, View, ScrollView } from 'react-native';
+import Api from '../../services/Api';
 import { useFonts, VT323_400Regular } from '@expo-google-fonts/vt323';
 import diaBackground from '../../../assets/background/dia.png';
 import noiteBackground from '../../../assets/background/noite.png';
 import { Divider } from 'react-native-paper';
+import { Feather } from '@expo/vector-icons';
 
-export default function Weather({navigation, route}) {
-  const latitude = route.params.latitude
-  const longitude = route.params.longitude
-  const imageBackground = route.params.imageBackground
-  console.log(latitude)
-  console.log(longitude)
+export default function Weather({ navigation, route }) {
+  const latitude = route.params.latitude;
+  const longitude = route.params.longitude;
+  const imageBackground = route.params.imageBackground;
+  console.log(latitude);
+  console.log(longitude);
 
-  const [apiData, setApiData] = useState(null); 
+  const [apiData, setApiData] = useState(null);
 
-  useEffect(() =>{
+  useEffect(() => {
     Api.get(`/forecast.json?q=${latitude},${longitude}&lang=pt`)
-    .then(res =>{ 
-      setApiData(res.data)
-    })
-  }, [latitude, longitude])
+      .then(res => {
+        setApiData(res.data);
+      });
+  }, [latitude, longitude]);
 
-  
   let [fontsLoaded] = useFonts({
     VT323_400Regular,
   });
@@ -31,103 +31,113 @@ export default function Weather({navigation, route}) {
     return null;
   }
 
-console.log(apiData)
+  const isDay = imageBackground === 'dia';
+
+  const getBackgroundColor = () => {
+    if (isDay) {
+      return { backgroundColor: '#74cae3', textColor: '#fff' }; // Cores para o dia
+    } else {
+      return { backgroundColor: '#81007f', textColor: '#fff' }; // Cores para a noite
+    }
+  };
+
+  console.log(apiData);
   return (
-    <ImageBackground source={imageBackground === 'dia' ? diaBackground : noiteBackground} style={styles.imageBackground}>
+    <ImageBackground source={isDay ? diaBackground : noiteBackground} style={styles.imageBackground}>
       <View style={styles.container}>
         <View>
-              <Text style={{ fontFamily: 'VT323_400Regular', fontSize: 25, color: 'white', paddingBottom: 0, paddingLeft: 20, marginTop: 50 }}>{apiData?.location?.name}, {apiData?.location?.region}</Text>
-              <View style={styles.temperaturas}>
-                <Text style={{ fontFamily: 'VT323_400Regular', fontSize: 90, color: 'white', paddingTop: 0, textAlign: 'center' }}>{apiData?.current?.feelslike_c}°</Text>
-                <Divider style={{ width: 1, height: '50%' }} />
-                <View>
-                  <Text style={{ fontFamily: 'VT323_400Regular', fontSize: 25, color: 'white', paddingBottom: 0 }}> {apiData?.forecast?.forecastday?.[0]?.day?.maxtemp_c}°</Text>
-                  <Text style={{ fontFamily: 'VT323_400Regular', fontSize: 25, color: '#bcbcbc', paddingBottom: 0 }}> {apiData?.forecast?.forecastday?.[0]?.day?.mintemp_c}°</Text>
-                </View>
-              </View>
-              <Text style={{ fontFamily: 'VT323_400Regular', fontSize: 30, color: '#bcbcbc', paddingTop: 0, textAlign: 'center' }}>{apiData?.current?.condition.text}</Text>
-
-              <View style={styles.info}>
-                <View style={styles.viewInfo}>
-                    <Text style={styles.texto}>
-                      {apiData?.current?.wind_kph}km/h
-                    </Text>
-                </View>
-
-                <View  style={styles.viewInfo}>
-                    <Text style={styles.texto}>
-                      {apiData?.current?.humidity}%
-                    </Text>
-                </View>
-
-                <View style={styles.viewInfo}>
-                    <Text style={styles.texto}>
-                    {apiData?.current?.precip_mm}mm
-                    </Text>
-                </View>
+          <Text style={{ fontFamily: 'VT323_400Regular', fontSize: 25, color: 'white', paddingBottom: 0, paddingLeft: 20, marginTop: 50 }}>{apiData?.location?.name}, {apiData?.location?.region}</Text>
+          <View style={styles.temperaturas}>
+            <Text style={{ fontFamily: 'VT323_400Regular', fontSize: 90, color: 'white', paddingTop: 0, textAlign: 'center' }}>{apiData?.current?.feelslike_c}°</Text>
+            <Divider style={{ width: 1, height: '50%' }} />
+            <View>
+              <Text style={{ fontFamily: 'VT323_400Regular', fontSize: 25, color: 'white', paddingBottom: 0 }}> {apiData?.forecast?.forecastday?.[0]?.day?.maxtemp_c}°</Text>
+              <Text style={{ fontFamily: 'VT323_400Regular', fontSize: 25, color: '#bcbcbc', paddingBottom: 0 }}> {apiData?.forecast?.forecastday?.[0]?.day?.mintemp_c}°</Text>
             </View>
-        </View>
+          </View>
+          <Text style={{ fontFamily: 'VT323_400Regular', fontSize: 30, color: '#bcbcbc', paddingTop: 0, textAlign: 'center' }}>{apiData?.current?.condition.text}</Text>
 
+          <View style={styles.info}>
+            <View style={[styles.viewInfo, { backgroundColor: getBackgroundColor().backgroundColor }]}>
+              <Feather name="wind" size={30} color={getBackgroundColor().textColor} />
+              <Text style={styles.textoPequeno}>
+                {apiData?.current?.wind_kph} km/h
+              </Text>
+            </View>
+
+            <View style={[styles.viewInfo, { backgroundColor: getBackgroundColor().backgroundColor }]}>
+              <Feather name="thermometer" size={30} color={getBackgroundColor().textColor} />
+              <Text style={styles.texto}>
+                {apiData?.current?.temp_c}°
+              </Text>
+            </View>
+
+            <View style={[styles.viewInfo, { backgroundColor: getBackgroundColor().backgroundColor }]}>
+              <Feather name="droplet" size={30} color={getBackgroundColor().textColor} />
+              <Text style={styles.texto}>
+                {apiData?.current?.humidity}%
+              </Text>
+            </View>
+          </View>
+        </View>
 
         <View style={styles.viewHoras}>
           <ScrollView style={styles.infoPorHoras}>
-          {apiData?.forecast?.forecastday?.[0]?.hour.map((hora) => 
-          <>
-            <Text key={hora.time_epoch}>{hora.time}</Text>
-            <Text key={hora.temp_c}>{hora.temp_c}</Text>
-          </>
-          )}
-        </ScrollView>
+            {apiData?.forecast?.forecastday?.[0]?.hour.map((hora) =>
+              <View key={hora.time_epoch}>
+                <Text>{hora.time}</Text>
+                <Text>{hora.temp_c}</Text>
+              </View>
+            )}
+          </ScrollView>
         </View>
 
         <View style={styles.butao}>
-            <TouchableOpacity 
-              onPress={()=>{ navigation.navigate('Home')}}
+          <TouchableOpacity
+            onPress={() => { navigation.navigate('Home') }}
             style={styles.buttonLarge}>
-              <ImageBackground
-                style={styles.imagebutao}
-                  source={require(`../../../assets/button.png`)}
-              >
-                <Text style={styles.buttonText}>Voltar</Text>
-              </ImageBackground>
-            </TouchableOpacity>
-
-          </View>
-    </View>
+            <ImageBackground
+              style={styles.imagebutao}
+              source={require(`../../../assets/button.png`)}
+            >
+              <Text style={styles.buttonText}>Voltar</Text>
+            </ImageBackground>
+          </TouchableOpacity>
+        </View>
+      </View>
     </ImageBackground>
- 
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    justifyContent:'space-between',
+  container: {
+    flex: 1,
+    justifyContent: 'space-between',
   },
   imageBackground: {
     flex: 1,
     resizeMode: 'cover',
     justifyContent: 'center',
-    width: '100%'
+    width: '100%',
   },
-  imagebutao:{
+  imagebutao: {
     resizeMode: 'cover',
     justifyContent: 'center',
     alignItems: 'center',
     width: 200,
     height: 200,
   },
-  butao:{
+  butao: {
     position: 'absolute',
     right: -30,
     bottom: -70,
   },
-  buttonText:{
+  buttonText: {
     color: '#fff',
     fontSize: 30,
     fontFamily: 'VT323_400Regular',
     top: -30,
-    left: -1
+    left: -1,
   },
   temperaturas: {
     flexDirection: 'row',
@@ -135,9 +145,15 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   texto: {
-    fontFamily: 'VT323_400Regular', 
-    fontSize: 25, 
-    color: '#fff',  
+    fontFamily: 'VT323_400Regular',
+    fontSize: 25,
+    color: '#fff',
+    textAlign: 'center',
+  },
+  textoPequeno: {
+    fontFamily: 'VT323_400Regular',
+    fontSize: 20,
+    color: '#fff',
     textAlign: 'center',
   },
   info: {
@@ -147,26 +163,26 @@ const styles = StyleSheet.create({
     gap: 50,
     paddingLeft: 50,
     paddingRight: 50,
-    marginTop: 40
+    marginTop: 40,
   },
   viewInfo: {
     width: 90,
     height: 90,
-    backgroundColor: "#7758D1",
-    borderRadius: 100 / 2,
+    backgroundColor: '#7758D1', 
+    borderRadius: 45,
     justifyContent: "center",
-    alignItems: "center"
+    alignItems: "center",
   },
   infoPorHoras: {
-      backgroundColor: "#7758D1",
-      width: '80%',
-      height: 500,
-      marginTop: 30,
-      borderRadius: 20,
-      flexDirection: 'row'
+    backgroundColor: "#7758D1",
+    width: '80%',
+    height: 500,
+    marginTop: 30,
+    borderRadius: 20,
+    flexDirection: 'row',
   },
   viewHoras: {
     justifyContent: "center",
-    alignItems: "center"
-  }
-})
+    alignItems: "center",
+  },
+});
